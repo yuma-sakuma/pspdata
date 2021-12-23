ef19a0589bf0b9b511ab67bad3eb5898f744df6d
yuma-sakuma <pgm15909@gmail.com>
2021-12-23 15:51:35 +0700

    backup

--- oopcard2.py
+++ oopcard2.py
@@ -9,14 +9,20 @@ class Game:
         self.powerP2 = 0
         if len(self.player1) == 3:
             if self.player1[0].suit == self.player1[1].suit == self.player1[2].suit:
-                self.power += 2
+                self.powerP1 += 2
             elif self.player1[0].suit == self.player1[1].suit or self.player1[1].suit == self.player1[2].suit or self.player1[1].suit == self.player1[2].suit:
-                self.power += 1
+                self.powerP1 += 1
+        else:
+            if self.player1[0].suit == self.player1[0].suit:
+                self.powerP1 += 1
         if len(self.player2) == 3:
             if self.player2[0].suit == self.player2[1].suit == self.player2[2].suit:
-                self.power += 2
+                self.powerP2 += 2
             elif self.player2[0].suit == self.player2[1].suit or self.player2[1].suit == self.player2[2].suit or self.player2[1].suit == self.player2[2].suit:
-                self.power += 1
+                self.powerP2 += 1
+        else:
+            if self.player2[0].suit == self.player2[0].suit:
+                self.powerP2 += 1
         for i in self.player1:
             if i.value == "K" or i.value == "Q" or i.value == "J":
                 self.valueP1 += 0
@@ -33,9 +39,17 @@ class Game:
             self.valueP2 -= 10
     def dual(self):
         if self.valueP1 > self.valueP2:
-            print("Player 1 Win :",self.player1)
+            if len(self.player1) == 3:
+                print("Player 1 win : {}{} , {}{} , {}{}".format(self.player1[0].value,self.player1[0].suit,self.player1[1].value,self.player1[1].suit,self.player1[2].suit,self.player1[2].value))
+            else:
+                print("Player 1 win : {}{} , {}{}".format(self.player1[0].value,self.player1[0].suit,self.player1[1].value,self.player1[1].suit))
         elif self.valueP2 > self.valueP1:
-            print("Player 2 win : {}{} and {}{}".format(self.player2[0].value,self.player2[0].suit,self.player2[1].value,self.player2[1].suit))
+            if len(self.player1) == 3:
+                print("Player 1 win : {}{} , {}{} , {}{}".format(self.player1[0].value,self.player1[0].suit,self.player1[1].value,self.player1[1].suit,self.player1[2].suit,self.player1[2].value))
+            else:
+                print("Player 2 win : {}{} , {}{}".format(self.player2[0].value,self.player2[0].suit,self.player2[1].value,self.player2[1].suit))
+        elif self.valueP1 == self.valueP2:
+            print("Draw")
 class Card:
     def __init__(self,suit,value):
         self.suit = suit
@@ -75,13 +89,25 @@ class Player:
     def cardsInHand(self):
         return self.cardsIH
 deck = Deck()
-player1 = Player("Char",deck.firstturn())
-player2 = Player("Pukkerr",deck.firstturn())
+p1 = input("Enter name Player 1 : ")
+p2 = input("Enter name Player 2 : ")
+player1 = Player(p1,deck.firstturn())
+player2 = Player(p2,deck.firstturn())
+print("{} card : {}".format(player1.player_name,player1.cardsIH))
+drawcardP1 = input("Do you want to draw card? : ")
+if drawcardP1 == "Yes":
+    player1.drawcard()
+    print("Player1 has draw {}".format(player1.cardsIH))
+    print("Turn player 2")
+else:
+    print("Turn player 2")
+print("{} card : {}".format(player2.player_name,player2.cardsIH))
+drawcardP2 = input("Do you want to draw card? : ")
+if drawcardP2 == "Yes":
+    player2.drawcard()
+    print("Player2 has draw {}".format(player1.cardsIH))
+    print("Dual!!!")
+else:
+    print("Dual!!!")
 game = Game(player1.cardsInHand(),player2.cardsInHand())
-#player1.checkCIH()
-#player2.checkCIH()
-#game.test()
-#deck.show()
-#player.drawcard(deck.drawcard())
-#player.checkCIH()
 game.dual()
